55ce7face927dfb23944d7c19cfd843ad365ca30
Jon Karlsen <karlsen.jonarild@gmail.com>
2019-03-26 12:21:19 +0100

    Completely over-engineer a simple problem

---
+++ data.js
@@ -0,0 +1,45 @@
+const education = [
+	{
+		institution: "OpenClassrooms",
+		subheading: "Back-End Web Developer: .NET",
+		bodyText:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		articleDate: {
+			date: "2019-2020",
+			place: "Oslo, Norway"
+		}
+	},
+	{
+		institution: "OpenClassrooms",
+		subheading: "Back-End Web Developer: .NET",
+		bodyText:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		articleDate: {
+			date: "2019-2020",
+			place: "Oslo, Norway"
+		}
+	}
+];
+
+const experience = [
+	{
+		institution: "OpenClassrooms",
+		subheading: "Back-End Web Developer: .NET",
+		bodyText:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		articleDate: {
+			date: "2019-2020",
+			place: "Oslo, Norway"
+		}
+	},
+	{
+		institution: "OpenClassrooms",
+		subheading: "Back-End Web Developer: .NET",
+		bodyText:
+			"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.",
+		articleDate: {
+			date: "2019-2020",
+			place: "Oslo, Norway"
+		}
+	}
+];
--- index.js
+++ index.js
@@ -4,51 +4,71 @@ const render = (template, node) => {
 	node.innerHTML = template;
 };
 
-const infoArticleTemplate = `<article class="info-article">
-<header class="article-header">
-    <h2 class="article-heading">
-        Education
-    </h2>
-</header>
+const renderEducation = data => {
+	let template = `<header class="article-header">
+            <h2 class="article-heading">
+                Education
+            </h2>
+        </header>`;
 
-<section class="article-body">
-    <div class="sub-article">
-        <h4 class="body-subheading">
-            OpenClassrooms
-        </h4>
-        <p class="body-subheading-subtitle">
-            Back-End Web Developer: .NET
-        </p>
-        <p class="body-text">
-            Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Culpa ipsam eveniet
-            aspernatur quae sed cumque minus unde, tempora
-            minima voluptatem.
-        </p>
-        <div class="article-date">
-            <p>2019-2020</p>
-            <p>Oslo, Norway</p>
-        </div>
-    </div>
-    <div class="sub-article">
-        <h4 class="body-subheading">
-            OpenClassrooms
-        </h4>
-        <p class="body-subheading-subtitle">
-            Back-End Web Developer: .NET
-        </p>
-        <p class="body-text">
-            Lorem ipsum dolor sit amet consectetur
-            adipisicing elit. Culpa ipsam eveniet
-            aspernatur quae sed cumque minus unde, tempora
-            minima voluptatem.
-        </p>
-        <div class="article-date">
-            <p>2019-2020</p>
-            <p>Oslo, Norway</p>
-        </div>
-    </div>
-</section>
-</article>`;
+	data.map(item => {
+		template += `
+            <section class="article-body">
+                <div class="sub-article">
+                    <h4 class="body-subheading">
+                        ${item.institution}
+                    </h4>
+                    <p class="body-subheading-subtitle">
+                        ${item.subheading}
+                    </p>
+                    <p class="body-text">
+                        ${item.bodyText}
+                    </p>
+                    <div class="article-date">
+                        <p>${item.articleDate.date}</p>
+                        <p>${item.articleDate.place}</p>
+                    </div>
+                </div>
+            </section>
+    `;
+	});
 
-render(infoArticleTemplate, document.querySelector("#root"));
+	// Render result
+	render(template, document.querySelector("#education"));
+};
+
+const renderExperience = data => {
+	let template = `<header class="article-header">
+            <h2 class="article-heading">
+                Experience
+            </h2>
+        </header>`;
+
+	data.map(item => {
+		template += `
+            <section class="article-body">
+                <div class="sub-article">
+                    <h4 class="body-subheading">
+                        ${item.institution}
+                    </h4>
+                    <p class="body-subheading-subtitle">
+                        ${item.subheading}
+                    </p>
+                    <p class="body-text">
+                        ${item.bodyText}
+                    </p>
+                    <div class="article-date">
+                        <p>${item.articleDate.date}</p>
+                        <p>${item.articleDate.place}</p>
+                    </div>
+                </div>
+            </section>
+    `;
+	});
+
+	// Render result
+	render(template, document.querySelector("#experience"));
+};
+
+renderEducation(education);
+renderExperience(experience);
